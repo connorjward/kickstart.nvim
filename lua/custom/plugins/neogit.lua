562955de4624ba443d7480d3ea1a07124da669e9
Connor Ward <c.ward20@imperial.ac.uk>
2024-05-06 11:32:55 +0100

    Add neogit package

---
+++ lua/custom/plugins/neogit.lua
@@ -0,0 +1,10 @@
+-- See https://github.com/NeogitOrg/neogit
+return {
+  'NeogitOrg/neogit',
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- required
+    'sindrets/diffview.nvim', -- optional - Diff integration
+    'nvim-telescope/telescope.nvim',
+  },
+  config = true,
+}
