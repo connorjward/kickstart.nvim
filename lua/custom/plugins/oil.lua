ca6245ebb6f6b656907a6797471f6da129d4850a
Connor Ward <c.ward20@imperial.ac.uk>
2024-05-20 11:09:40 +0100

    Add oil plugin

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,11 @@
+-- https://github.com/stevearc/oil.nvim
+return {
+  'stevearc/oil.nvim',
+  opts = {},
+  -- Optional dependencies
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+  init = function()
+    require('oil').setup()
+  end,
+}
